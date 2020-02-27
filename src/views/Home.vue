7a2da8f41210686d529c9d6794c6b5633e9b29f1
Fredrik Vraalsen <fredrik.vraalsen@byr.oslo.kommune.no>
2020-02-27 11:02:49 +0100

    Grupper og korte beskrivelser

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="home">
-		<h1>Værsego'! Lag magi</h1>
+	<h1>Team Infrastruktur</h1>
+    <div>Selvbetjente tjenester for å hjelpe utviklere i Oslo kommune med effektiv utvikling og forvaltning av digitale løsninger.</div>
+
+    <h2>Bygg og deploy</h2>
+    <div>Automatisere testing, pakking og utrulling av applikasjoner og tjenester.</div>
+
+    <h2>Sikkerhet</h2>
+    <div>Sikre applikasjonen med innlogging både for ansatte og innbyggere.</div>
+
+    <h2>Lagring</h2>
+    <div>Enkel tilgang på sikker datalagring.</div>
+
+    <h2>Integrasjoner</h2>
+    <div>Koble sammen applikasjoner og tjenester med meldingskøer.</div>
+
+    <h2>Overvåkning</h2>
+    <div>Bedre kontroll på hvordan applikasjonen din oppfører seg i sanntid med dashboards, varsling og logger.</div>
   </div>
 </template>
 
