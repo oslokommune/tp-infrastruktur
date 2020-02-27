ad901dd3d8bcaf90075837bc18bf8e28b3e55199
Simen Harket Bakke <simenb@netcompany>
2020-02-27 12:21:01 +0100

    lagt til innhold i views

--- src/views/Lagring.vue
+++ src/views/Lagring.vue
@@ -1,15 +1,11 @@
 <template>
     <div class="home">
-        <h1 class="text-left">Sikkerhet</h1>
+        <h1 class="text-left">Fillagring</h1>
 
-        <img class="float-right" src="../assets/add-mapper.png" width="400px">
-        <h2 class="text-left">Ansattautentisering – Keycloak</h2>
+        <h2 class="text-left">Fillagring – Minio</h2>
 
-        <p class="text-left">Keycloak er en autentiseringstjeneste som tilbyr innlogging både for ansatte og innbyggere. Keycloak støtter OpenIDConnect og SAML 2.0, og gir single sign on på tvers av tjenester og protokoller. Om en ansatt skal logge inn må det kontrolleres organisasjonstilhørighet. Sjekken lages i appen og ikke Keycloak.</p>
+        <p class="text-left">En "komponent" vi tilbyr som skal gjøre det enkelt for hvert enkelt team å spinne opp en
+            fillagringstjeneste i sine egne namespaces, basert på MinIO. Denne kan brukes i stedet for dokudb.</p>
 
-        <h2 class="text-left">Innbyggerautentisering – Keycloak</h2>
-
-        <img class="float-right" src="../assets/default-groups.png" width="400px">
-        <p class="text-left">Keycloak er en autentiseringstjeneste som tilbyr innlogging både for ansatte og innbyggere. Keycloak støtter OpenIDConnect og SAML 2.0, og gir single sign on på tvers av tjenester og protokoller. Innbyggere videresendes til ID-porten.</p>
     </div>
 </template>
---
+++ src/views/Overvakning.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="home">
+        <h1 class="text-left">Overvåkning</h1>
+
+        <h2 class="text-left">Overvåkning - Grafana</h2>
+
+        <p class="text-left">Overvåkningsløsning for komponenter som kjører på kjøremiljøene driftet av Oslo kommune.
+            Løsningen består av et dashboard i Grafana, og en Slack-app som varsler om hendelser – både overvåkning- og
+            loggvarsler. Målgruppen er utviklerteam i Oslo kommune, per i dag begrenset til team som benytter Oslo
+            kommune sin tjenesteplattform. </p>
+
+
+        <h2 class="text-left">Metrics - Prometheus</h2>
+
+        <p class="text-left">Metrics er tallbaserte data som beskriver en egenskap ved et system på et gitt tidspunkt.
+            Ved å undersøke ulike metrics om et system, og sjekke hvordan verdiene forandrer seg over tid, kan man få et
+            godt innblikk i hvordan systemet oppfører seg.</p>
+
+    </div>
+</template>
