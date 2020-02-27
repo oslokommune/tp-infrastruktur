80eec8d24544643525f75c1735dffc4c104b5f91
Fredrik Vraalsen <fredrik.vraalsen@byr.oslo.kommune.no>
2020-02-27 12:31:42 +0100

    Overskrift sikkerhet

--- src/views/Sikkerhet.vue
+++ src/views/Sikkerhet.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
     <v-breadcrumbs :items="items"></v-breadcrumbs>
+    <h2 class="text-left"><v-icon large>mdi-security</v-icon> Sikkerhet</h2>
 
-    <img class="float-right" src="../assets/add-mapper.png" width="400px">
-    <h2 class="text-left">Ansattautentisering – Keycloak</h2>
-
+    <img class="float-right" src="../assets/add-mapper.png" width="300px">
+    <h3 class="text-left mb-1">Ansattautentisering – Keycloak</h3>
     <p class="text-left">Keycloak er en autentiseringstjeneste som tilbyr innlogging både for ansatte og innbyggere.
       Keycloak støtter OpenIDConnect og SAML 2.0, og gir single sign on på tvers av tjenester og protokoller. Om en
       ansatt skal logge inn må det kontrolleres organisasjonstilhørighet. Sjekken lages i appen og ikke Keycloak.</p>
 
-    <h2 class="text-left">Innbyggerautentisering – Keycloak</h2>
-
-    <img class="float-right" src="../assets/default-groups.png" width="400px">
+    <img class="float-right" src="../assets/default-groups.png" width="300px">
+    <h3 class="text-left mb-1">Innbyggerautentisering – Keycloak</h3>
     <p class="text-left">Keycloak er en autentiseringstjeneste som tilbyr innlogging både for ansatte og innbyggere.
       Keycloak støtter OpenIDConnect og SAML 2.0, og gir single sign on på tvers av tjenester og protokoller. Innbyggere
       videresendes til ID-porten.</p>
