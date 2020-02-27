1c68adcd7764e0f123c92e18bc15cae98eda9b7a
Fredrik Vraalsen <fredrik.vraalsen@byr.oslo.kommune.no>
2020-02-27 12:44:09 +0100

    Datasett besøkshistorikk

--- src/views/Overvakning.vue
+++ src/views/Overvakning.vue
@@ -10,13 +10,14 @@
             loggvarsler. Målgruppen er utviklerteam i Oslo kommune, per i dag begrenset til team som benytter Oslo
             kommune sin tjenesteplattform. </p>
 
-
         <h3 class="text-left">Metrics - Prometheus</h3>
 
         <p class="text-left">Metrics er tallbaserte data som beskriver en egenskap ved et system på et gitt tidspunkt.
             Ved å undersøke ulike metrics om et system, og sjekke hvordan verdiene forandrer seg over tid, kan man få et
             godt innblikk i hvordan systemet oppfører seg.</p>
 
+        <h3 class="text-left">Historiske besøksdata</h3>
+        <p class="text-left"><a href="https://apis.k8s-test.oslo.kommune.no/katalog/data/besokshistorikk-tjenester">Datasett</a> med statistikk over unike besøk per tjeneste</p>
     </div>
 </template>
 
