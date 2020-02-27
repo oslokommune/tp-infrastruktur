89a5316fb6be78272352a3b471ab7d64245a17e0
Fredrik Vraalsen <fredrik.vraalsen@byr.oslo.kommune.no>
2020-02-27 14:41:32 +0100

    Legg til Clippy the Chatbot

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,16 +4,34 @@
     <p class="text-left">Selvbetjente tjenester for å hjelpe utviklere i Oslo kommune med effektiv utvikling og forvaltning av digitale løsninger.</p>
     <tjenesteoversikt/>
 
+    <h3 class="text-left">Spørsmål?</h3>
+    <p class="text-left">Kontakt oss på Slack <a href="https://app.slack.com/client/T6W3G5A4C/C6XTG3FGW">#team-infrastruktur</a> eller spør vår chatbot Clippy!
+      <TextInput style="display: inline-block; margin-right: 1em;" v-model="botQuestion" />
+      <BaseButton primary style="margin: 1em 0 1em 0;">Spør Clippy!</BaseButton>
+      <img alt="clippy" src="https://media.giphy.com/media/13V60VgE2ED7oc/giphy.gif" class="float-right"/>
+    </p>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
+import { BaseButton } from '@tuuturu/vue/buttons' // Knappene finnes her
+import { TextInput } from '@tuuturu/vue/forms' // Skjema-komponenter her
+
 import Tjenesteoversikt from "../components/Tjenesteoversikt";
+
 export default {
   name: 'Home',
-  components: {Tjenesteoversikt}
+  components: {
+    BaseButton,
+    TextInput,
+    Tjenesteoversikt
+  },
+  data: () => ({
+    botQuestion: "Spørsmål"
+  })
 }
 </script>
 
